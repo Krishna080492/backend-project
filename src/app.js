5c3eef2158c893b892a8dadfc8d974272472ddd9
Krishna080492 <krishna.desai4@gmail.com>
2025-04-10 16:33:02 +0530

    util codes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+// config
+app.use(express.json({ limit: "16kb" })); //for json
+app.use(express.urlencoded({ extended: true })); //for url
+app.use(express.static("public")); //for public access
+app.use(cookieParser()); //for user cookies access - for crud operation
+export { app };
